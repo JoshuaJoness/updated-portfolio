a05ebe3adb2c8ba278648a129768de217ff1ab01
Joshua Jones <joshuajonesdev@gmail.com>
2020-12-10 19:27:35 -0500

    created mesh, added to canvas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
+import { Canvas } from 'react-three-fiber'
+import { OrbitControls, Stars } from 'drei'
 import './style.css'
 
+const Box = () => {
+  const [hover, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  return (
+    <mesh 
+      onPointerOver={() => setHover(true)} 
+      onPointerOut={() => setHover(false)}
+      onClick={() => setActive(!active)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshLambertMaterial attach="material" color={hover ? "grey" : "hotpink"} />
+    </mesh>
+  )
+}
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Canvas>
+    <OrbitControls />
+    <Stars />
+    <ambientLight intensity={0.5} />
+    <spotLight position={[10, 15, 10]} angle={0.3} />
+  <Box />
+  </Canvas>
+  )
+
 }
