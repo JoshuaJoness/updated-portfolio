2f39079ef0f40b2ad2a8aeeeed7f3b74a041648a
Joshua Jones <joshuajonesdev@gmail.com>
2020-12-11 21:05:56 -0500

    model now loaded using Drei, rendering better

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+plugins: [
+    // {
+    //   resolve: `gatsby-plugin-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Mukta Malar`
+    //       },
+    //     ],
+    //   },
+    // }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,60 @@
-import React, { useState, useRef } from "react"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, useFrame, extend, useThree } from 'react-three-fiber'
-import { Stars } from 'drei'
+import React, { useState, useRef, useEffect, Suspense } from "react"
+import * as THREE from 'three'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Canvas, extend, useFrame, useThree } from 'react-three-fiber'
+import { useGLTF, OrbitControls, Stars, draco } from 'drei'
 import { useSpring, a } from 'react-spring/three'
 import './style.css'
 
-extend({ OrbitControls })
 
-const Controls = () => {
-  const orbitRef = useRef()
-  const { camera, gl } = useThree()
-
-  useFrame(() => {
-    orbitRef.current.update()
-  })
-
-  return (
-    <orbitControls autoRotate /* maxPolarAngle={Math.PI / 3} minPolarAngle={Math.PI / 3} */ args={[camera, gl.domElement]} ref={orbitRef} />
-  )
+const Samurai = () => {
+  const gltf = useGLTF('/scene.gltf', true)
+  return <primitive object={gltf.scene} dispose={null} />
 }
+
+// extend({ OrbitControls })
+
+// const Samurai = () => {
+//   const [model, setModel] = useState()
+
+//   useEffect(() => {
+//     new GLTFLoader().load('/scene.gltf', setModel)
+//   })
+
+//   return model ? <primitive object={model.scene} position={[0, -30, -300]} /> : null
+// }
+
+// const Controls = () => {
+//   const orbitRef = useRef()
+//   const { camera, gl } = useThree()
+
+//   useFrame(() => {
+//     orbitRef.current.update()
+//   })
+
+//   return (
+//     <orbitControls 
+//       autoRotate 
+//       /* maxPolarAngle={Math.PI / 3} 
+//       minPolarAngle={Math.PI / 3} */ 
+//       args={[camera, gl.domElement]} 
+//       ref={orbitRef} 
+//     />
+//   )
+// }
+
+
+const Plane = () => (
+  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <a.meshPhysicalMaterial attach="material" color="white" />
+  </mesh>
+)
+
+
+
+
 
 const Box = () => {
   const meshRef = useRef()
@@ -44,23 +80,56 @@ const Box = () => {
       onPointerOut={() => setHover(false)}
       onClick={() => setActive(!active)}
       scale={props.scale}
+      castShadow
     >
+      {/* <ambientLight intensity={0.5} />
+      <spotLight position={[0, 5, 10]} penumbra={1} castShadow /> */}
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <a.meshBasicMaterial attach="material" color={props.color} />
+      {/* <Samurai /> */}
+      <a.meshPhysicalMaterial attach="material" color={props.color} />
     </a.mesh>
   )
 }
 
 export default function Home() {
   return (
-    <Canvas>
-      {/* <OrbitControls /> */}
-      <Controls />
-      <Stars />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Box />
-  </Canvas>
-  )
+    <> 
+      <div style={{display:'flex', justifyContent:'space-between', position: 'absolute', top: 0, left: 0}}>
+        <button style={{border:'none', margin: 20, background: 'none', fontSize: 39, height: 10}} onClick={() => console.log('testing...')}>PROJECTS</button>
+        <button style={{border:'none', margin: 20, background: 'none', fontSize: 39, height: 10}} onClick={() => console.log('testing...')}>CONTACT</button>
+      </div>
 
+      <Canvas 
+        camera={{ position: [0, 0, 5] }} 
+        onCreated={({ gl }) => { 
+          gl.shadowMap.enabled = true
+          gl.shadowMap.type = THREE.PCFSoftShadowMap
+        }}
+      >
+        <ambientLight intensity={0.5} />
+        <spotLight position={[0, 5, 10]} penumbra={1} castShadow />
+        <OrbitControls />     
+        {/* <Controls /> */}
+        <Suspense fallback={null}>
+          <mesh position={[0, -30, -300]}>
+            <Samurai />
+          </mesh>
+        </Suspense>
+
+        {/* <Samurai /> */}
+        {/* <Box /> */}
+        {/* <fog attach="fog" args={["white", 15, 20]} /> */}
+           
+        {/* <Stars />        */}
+        {/* <Plane /> */}
+    </Canvas>
+  </>
+  )
 }
+
+// TODO 
+// import model
+// add fonts (Typographpy)
+// add music (Howler?)
+// custom cursor
+// add project pages
